0bc8cd03b8b1a17746434dbbd8f8350be77bae18
Landvoigt <tim.voigt@ik.me>
2023-03-28 01:14:27 +0200

    fixed all mistakes

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
 let pokemon;
-let pokemonBefore;
-let pokemonAfter;
 let AUDIO_FRONTPAGE = new Audio('./audio/frontpage/poke_center.mp3');
 let types = [
     {
@@ -97,38 +95,15 @@ let types = [
 let pokemonArray = [];
 
 async function loadPokedex() {
-    for (let x = 1; x < 100; x++) {
-        if (x == 1) {
-            let y = x;
-            let z = x + 1;
-            let pokemonURL = `https://pokeapi.co/api/v2/pokemon/${x}`;
-            let urlPokemonBefore = `https://pokeapi.co/api/v2/pokemon/${y}`;
-            let urlPokemonAfter = `https://pokeapi.co/api/v2/pokemon/${z}`;
-            let response = await fetch(pokemonURL);
-            let responseBefore = await fetch(urlPokemonBefore);
-            let responseAfter = await fetch(urlPokemonAfter);
-            pokemon = await response.json();
-            pokemonBefore = await responseBefore.json();
-            pokemonAfter = await responseAfter.json();
-            // console.log(pokemon, pokemonBefore, pokemonAfter);
-            variables(pokemon, pokemonBefore, pokemonAfter, x);
-        } else {
-            let y = x - 1;
-            let z = x + 1;
-            let pokemonURL = `https://pokeapi.co/api/v2/pokemon/${x}`;
-            let urlPokemonBefore = `https://pokeapi.co/api/v2/pokemon/${y}`;
-            let urlPokemonAfter = `https://pokeapi.co/api/v2/pokemon/${z}`;
-            let response = await fetch(pokemonURL);
-            let responseBefore = await fetch(urlPokemonBefore);
-            let responseAfter = await fetch(urlPokemonAfter);
-            pokemon = await response.json();
-            pokemonBefore = await responseBefore.json();
-            pokemonAfter = await responseAfter.json();
-            // console.log(pokemon, pokemonBefore, pokemonAfter);
-            variables(pokemon, pokemonBefore, pokemonAfter, x);
-        }
+    pokemonArray.length = 0;
+    for (let x = 1; x < 20; x++) {
+        let pokemonURL = `https://pokeapi.co/api/v2/pokemon/${x}`;
+        let response = await fetch(pokemonURL);
+        pokemon = await response.json();
+        variables(pokemon, x);
     }
     showStartButton();
+    console.log(pokemon);
 }
 
 function showStartButton() {
@@ -145,31 +120,31 @@ function showStartAnimation() {
     setTimeout(deleteFrontPage, 2500);
 }
 
-function variables(pokemon, y, z, x) {
+function variables(pokemon, x) {
     let name = pokemon['name'];
     let pic = pokemon['sprites']['other']['home']['front_default'];
     let card = document.getElementById('mainContent');
     if (pokemon['types'].length == 1) {
         let type_1 = pokemon['types'][0]['type']['name'];
-        updateTypeColorSingular(name, pic, type_1, card, x, y, z);
+        updateTypeColorSingular(name, pic, type_1, card, x);
     } else {
         let type_1 = pokemon['types'][0]['type']['name'];
         let type_2 = pokemon['types'][1]['type']['name'];
-        updateTypeColorMultiple(name, pic, type_1, type_2, card, x, y, z);
+        updateTypeColorMultiple(name, pic, type_1, type_2, card, x);
     }
 }
 
-function updateTypeColorSingular(name, pic, type_1, card, x, y , z) {
+function updateTypeColorSingular(name, pic, type_1, card, x) {
     for (let i = 0; i < types.length; i++) {
         let color = types[i]['type'];
         let typecolor = types[i]['color'];
         if (type_1 == color) {
-            renderPokemon(name, pic, type_1, card, typecolor, x, y, z)
+            renderPokemon(name, pic, type_1, card, typecolor, x)
         }
     }
 }
 
-function updateTypeColorMultiple(name, pic, type_1, type_2, card, x, y, z) {
+function updateTypeColorMultiple(name, pic, type_1, type_2, card, x) {
     for (let i = 0; i < types.length; i++) {
         let firstcolor = types[i]['type'];
         if (type_1 == firstcolor) {
@@ -178,109 +153,38 @@ function updateTypeColorMultiple(name, pic, type_1, type_2, card, x, y, z) {
                 let secondcolor = types[j]['type'];
                 if (type_2 == secondcolor) {
                     let typecolor2 = types[j]['color'];
-                    renderPokemon2(name, pic, type_1, type_2, card, typecolor1, typecolor2, x, y, z);
+                    renderPokemon2(name, pic, type_1, type_2, card, typecolor1, typecolor2, x);
                 }
             }
         }
     }
 }
 
-function renderPokemon(name, pic, type_1, card, typecolor, x, y, z) {
+function renderPokemon(name, pic, type_1, card, typecolor, x) {
     let index = pokemon['id'];
-    let test = pokemon[x];
     if (index < 10) {
         index = '00' + index;
     }
     if (index > 9 && index < 100) {
         index = '0' + index;
     }
-    let shinyPic = pokemon['sprites']['other']['dream_world']['front_default'];
-    let picBefore = y['sprites']['other']['dream_world']['front_default'];
-    let picAfter = z['sprites']['other']['dream_world']['front_default'];
     card.innerHTML += `
         <div class="poke-card" onclick="showBigPokemonCard(${x})">
-        <img src="${pic}" class="small-pokemon-img">
-        <div class="card-bg-one-type" style="background-color: ${typecolor}">
-        </div>
-        <div class="card-text">
-        <div class="card-text-top">
-        <h3 class="text-center mt-40">${name}</h3>
-        </div>
-        <div class="card-text-bottom">
-        <i class="index-number text-center"> #${index}</i>
-        <span id="type${x}" class="type-small-image">${type_1}</span>
-        </div>
-        </div>
-        </div>
-        <div id="bigCardWindow${x}" class="big-card-window d-none" onclick="hideBigPokemonCard(${x})">
-        <img src="./img/icons/arrow_left.png" id="bigImage" class="arrow a-left" onclick="stopProp()">
-        <div class="big-poke-card-container">
-        <div id="bigCardLeft${x}" class="poke-card big-poke-card-left" onclick="stopProp()">
-            <img src="./img/pokeball_bg_grey_3.png" class="big-card-bg">
-            <img src="${picBefore}" class="big-pokemon-img">
-            <div class="big-card-bg-left" style="background-color: ${typecolor}">
-            </div>
-            <div class="big-card-bg-right" style="background-color: ${typecolor}">
-            </div>
-            <div class="big-card-text">
+            <img src="${pic}" class="small-pokemon-img">
+            <div class="card-bg-one-type" style="background-color: ${typecolor}"></div>
+            <div class="card-text">
                 <div class="card-text-top">
-                    <h3 class="big-card-pokemon-name">${name}</h3>
-                    <i class="index-number big-card-index-number"> #${index}</i>
-                    <span class="type-small-image big-card-type">${type_1}</span>
+                    <h3 class="text-center mt-40">${name}</h3>
+                </div>
+                <div class="card-text-bottom">
+                    <i class="index-number text-center"> #${index}</i>
+                    <span id="type${x}" class="type-small-image">${type_1}</span>
                 </div>
             </div>
-            <div class="big-card-details-bg-1"></div>
-            <div class="big-card-details-bg-2"></div>
-            <div class="big-card-details-bg-3"></div>
-            <div class="big-card-details-bg-4">
-                <div class="pokemon-details"></div>
-            </div>
-        </div>
-        <div id="bigCardMain${x}" class="poke-card big-poke-card" onclick="stopProp()">
-            <img src="./img/pokeball_bg_grey_3.png" class="big-card-bg">
-            <img src="${shinyPic}" class="big-pokemon-img">
-            <div class="big-card-bg-left" style="background-color: ${typecolor}">
-            </div>
-            <div class="big-card-bg-right" style="background-color: ${typecolor}">
-            </div>
-            <div class="big-card-text">
-                <div class="card-text-top">
-                    <h3 class="big-card-pokemon-name">${name}</h3>
-                    <i class="index-number big-card-index-number"> #${index}</i>
-                    <span class="type-small-image big-card-type">${type_1}</span>
-                </div>
-            </div>
-            <div class="big-card-details-bg-1"></div>
-            <div class="big-card-details-bg-2"></div>
-            <div class="big-card-details-bg-3"></div>
-            <div class="big-card-details-bg-4">
-                <div class="pokemon-details"></div>
-            </div>
-        </div>
-        <div id="bigCardRight${x}" class="poke-card big-poke-card-right" onclick="stopProp()">
-            <img src="./img/pokeball_bg_grey_3.png" class="big-card-bg">
-            <img src="${picAfter}" class="big-pokemon-img">
-            <div class="big-card-bg-left" style="background-color: ${typecolor}">
-            </div>
-            <div class="big-card-bg-right" style="background-color: ${typecolor}">
-            </div>
-            <div class="big-card-text">
-                <div class="card-text-top">
-                    <h3 class="big-card-pokemon-name">${name}</h3>
-                    <i class="index-number big-card-index-number"> #${index}</i>
-                    <span class="type-small-image big-card-type">${type_1}</span>
-                </div>
-            </div>
-            <div class="big-card-details-bg-1"></div>
-            <div class="big-card-details-bg-2"></div>
-            <div class="big-card-details-bg-3"></div>
-            <div class="big-card-details-bg-4">
-                <div class="pokemon-details"></div>
-            </div>
-        </div>
-        <img src="./img/icons/arrow_right.png" id="" class="arrow a-right" onclick="stopProp()">
         </div>
         `;
+    let type_2 = '';
+    pushPokemonDetailsInJSON(x, name, index, pic, type_1, type_2, typecolor, typecolor);
 }
 
 function renderPokemon2(name, pic, type_1, type_2, card, typecolor1, typecolor2, x) {
@@ -291,56 +195,40 @@ function renderPokemon2(name, pic, type_1, type_2, card, typecolor1, typecolor2,
     if (index > 9 && index < 100) {
         index = '0' + index;
     }
-    let shinyPic = pokemon['sprites']['other']['dream_world']['front_default'];
     card.innerHTML += `
         <div class="poke-card" onclick="showBigPokemonCard(${x})">
-        <img src="${pic}" class="small-pokemon-img">
-        <div class="card-bg-left" style="background-color: ${typecolor1}">
-        </div>
-        <div class="card-bg-right" style="background-color: ${typecolor2}">
-        </div>
-        <div class="card-text">
-        <div class="card-text-top">
-        <span class="type-small-image">${type_1}</span>
-        <h3 class="text-center">${name}</h3>
-        </div>
-        <div class="card-text-bottom">
-        <i class="index-number text-center"> #${index}</i>
-        <span class="type-small-image">${type_2}</span>
-        </div>
-        </div>
-        </div>
-        <div id="bigCardWindow${x}" class="big-card-window d-none" onclick="hideBigPokemonCard(${x})">
-        <img src="./img/icons/arrow_left.png" class="arrow a-left" onclick="stopProp()">
-        <div class="big-poke-card-container">
-        <div id="bigCardLeft${x}" class="poke-card big-poke-card-left" onclick="stopProp()"></div>
-        <div id="bigCardMain${x}" class="poke-card big-poke-card" onclick="stopProp()">
-            <img src="./img/pokeball_bg_grey_3.png" class="big-card-bg">
-            <img src="${shinyPic}" class="big-pokemon-img">
-            <div class="big-card-bg-left" style="background-color: ${typecolor1}">
-            </div>
-            <div class="big-card-bg-right" style="background-color: ${typecolor2}">
-            </div>
-            <div class="big-card-text">
+            <img src="${pic}" class="small-pokemon-img">
+            <div class="card-bg-left" style="background-color: ${typecolor1}"></div>
+            <div class="card-bg-right" style="background-color: ${typecolor2}"></div>
+            <div class="card-text">
                 <div class="card-text-top">
-                    <h3 class="big-card-pokemon-name">${name}</h3>
-                    <i class="index-number big-card-index-number"> #${index}</i>
-                    <span class="type-small-image big-card-type">${type_1}</span>
-                    <span class="type-small-image big-card-type">${type_2}</span>
+                    <span class="type-small-image">${type_1}</span>
+                    <h3 class="text-center">${name}</h3>
+                </div>
+                <div class="card-text-bottom">
+                    <i class="index-number text-center"> #${index}</i>
+                    <span class="type-small-image">${type_2}</span>
                 </div>
             </div>
-            <div class="big-card-details-bg-1"></div>
-            <div class="big-card-details-bg-2"></div>
-            <div class="big-card-details-bg-3"></div>
-            <div class="big-card-details-bg-4">
-                <div class="pokemon-details"></div>
-            </div>
-        </div>
-        <div id="bigCardRight${x}" class="poke-card big-poke-card-right" onclick="stopProp()"></div>
-        </div>
-        <img src="./img/icons/arrow_right.png" class="arrow a-right" onclick="stopProp()">
         </div>
         `;
+    pushPokemonDetailsInJSON(x, name, index, pic, type_1, type_2, typecolor1, typecolor2);
+}
+
+function pushPokemonDetailsInJSON(x, name, index, pic, type_1, type_2, typecolor1, typecolor2) {
+    let shinyPic = pokemon['sprites']['other']['dream_world']['front_default'];
+    let data = {
+        'url': `https://pokeapi.co/api/v2/pokemon/${x}`,
+        'name': name,
+        'index': index,
+        'picSRC': pic,
+        'type1': type_1,
+        'type2': type_2,
+        'typecolor1': typecolor1,
+        'typecolor2': typecolor2,
+        'shinyPic' : shinyPic
+    }
+    pokemonArray.push(data);
 }
 
 function deleteFrontPage() {
@@ -351,34 +239,46 @@ function deleteFrontPage() {
     document.getElementById('body').classList.remove('overflow');
 }
 
-async function showBigPokemonCard(x) {
-    document.getElementById(`bigCardWindow${x}`).classList.remove('d-none');
-    // let left = document.getElementById(`bigCardLeft${x}`);
-    // let main = document.getElementById(`bigCardMain${x}`);
-    // let right = document.getElementById(`bigCardRight${x}`);
-    // console.log(pokemon, left, right, main);
-    // let name = pokemon['name'];
-    // let pic = pokemon['sprites']['other']['home']['front_default'];
-    // showPokemon(name, pic, left);
-    // showPokemon(name, pic, main);
-    // showPokemon(name, pic, right);
+function showBigPokemonCard(x) {
+    document.getElementById(`bigCardWindow`).classList.remove('d-none');
+    let main = document.getElementById(`bigCardMain`);
+    let numberPokemonMiddle = x - 1;
+    loadPokemonsDetails(numberPokemonMiddle, main);
+    let left = document.getElementById(`bigCardLeft`);
+    let numberPokemonLeft = x - 2;
+    loadPokemonsDetails(numberPokemonLeft, left);
+    let right = document.getElementById(`bigCardRight`);
+    let numberPokemonRight = x;
+    loadPokemonsDetails(numberPokemonRight, right);
 }
 
-function showPokemon(name, pic, position) {
+function loadPokemonsDetails(number, position) {
+    let pokemon = pokemonArray[number];
+    let name = pokemon['name'];
+    let index = pokemon['index'];
+    let pic = pokemon['shinyPic'];
+    let type1 = pokemon['type1'];
+    let type2 = pokemon['type2'];
+    let typecolor1 = pokemon['typecolor1'];
+    let typecolor2 = pokemon['typecolor2'];
+    showPokemon(name, index, pic, type1, type2, typecolor1, typecolor2, number, position);
+}
+
+function showPokemon(name, index, pic, type1, type2, typecolor1, typecolor2, number, position) {
     position.innerHTML =
         `
     <img src="./img/pokeball_bg_grey_3.png" class="big-card-bg">
     <img src="${pic}" class="big-pokemon-img">
-    <div class="big-card-bg-left" style="background-color: ">
+    <div class="big-card-bg-left" style="background-color: ${typecolor1}">
     </div>
-    <div class="big-card-bg-right" style="background-color: ">
+    <div class="big-card-bg-right" style="background-color: ${typecolor2}">
     </div>
     <div class="big-card-text">
         <div class="card-text-top">
             <h3 class="big-card-pokemon-name">${name}</h3>
-            <i class="index-number big-card-index-number"> #</i>
-            <span class="type-small-image big-card-type"></span>
-            <span class="type-small-image big-card-type"></span>
+            <i class="index-number big-card-index-number"> #${index}</i>
+            <span class="type-small-image big-card-type">${type1}</span>
+            <span id="secondType${number}" class="type-small-image big-card-type">${type2}</span>
         </div>
     </div>
     <div class="big-card-details-bg-1"></div>
@@ -388,10 +288,13 @@ function showPokemon(name, pic, position) {
         <div class="pokemon-details"></div>
     </div>
     `;
+    if (type2 == 0) {
+        document.getElementById(`secondType${number}`).classList.add('d-none');
+    }
 }
 
-function hideBigPokemonCard(x) {
-    document.getElementById(`bigCardWindow${x}`).classList.add('d-none');
+function hideBigPokemonCard() {
+    document.getElementById(`bigCardWindow`).classList.add('d-none');
 }
 
 function stopProp() {
